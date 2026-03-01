02e97358b892360bb11bdd450348c83e43c27360
tn11 <tn11@illinois.edu>
2026-03-01 01:13:42 -0600

    Added get chunk metadata

--- app/core/store_metadata.py
+++ app/core/store_metadata.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 from app.core.file_format import ChunkMetadata, FileMetadata
 from pymongo import AsyncMongoClient
 
+_uri = os.getenv("MONGO_URI")
+client = AsyncMongoClient(_uri)
 
-async def record_chunk_metadata(file_meta: FileMetadata, chunk_meta: ChunkMetadata):
+async def record_chunk_metadata(file_meta: FileMetadata, chunk_meta: ChunkMetadata) -> None:
     new_chunk_dict = chunk_meta.model_dump()
     file_base_dict = file_meta.model_dump(exclude={"chunks"}) # Exclude the empty list
 
@@ -27,19 +29,27 @@ async def record_chunk_metadata(file_meta: FileMetadata, chunk_meta: ChunkMetada
         raise Exception("Unable to find the document due to the following error: ", e)
     
 
+async def get_chunk_metadata(file_name: str) -> list[str]:
+    try:
+        database = client.get_database("unibase")
+        unibase = database.get_collection("unibase_collection")
+        query = { "_id": file_name }
 
-load_dotenv()
-_uri = os.getenv("MONGO_URI")
-client = AsyncMongoClient(_uri)
-test_file = FileMetadata(
-    file_name="test1.txt",
-    file_size=1,
-    chunk_size=20,
-)
-current_chunk = ChunkMetadata(
-    chunk_index=0,
-    db_provider="gd",
-    provider_id="gd_id_fake",
-    sha256="checksum"
-)
-asyncio.run(record_chunk_metadata(test_file, current_chunk))
+        # Fetch the document based on file_name
+        doc = await unibase.find_one(query)
+        if not doc: return []
+
+        # Extract the chunks and order by chunk_id
+        chunk_dict = doc.get("chunk_list", {})
+        raw_chunks = list(chunk_dict.values())
+        sorted_chunks = sorted(raw_chunks, key=lambda x: x["chunk_index"])
+
+        # List of chunk_id only
+        chunk_id_list = []
+        for chunk in sorted_chunks:
+            chunk_id_list.append(chunk["provider_id"])
+        
+        return chunk_id_list
+    except Exception as e:
+            raise Exception("Unable to find the document due to the following error: ", e)
+
