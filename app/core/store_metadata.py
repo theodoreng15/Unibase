32a26884c11d0ae6c895dd54aa532f0063752df6
tn11 <tn11@illinois.edu>
2026-03-01 01:23:53 -0600

    Rough delete function

--- app/core/store_metadata.py
+++ app/core/store_metadata.py
@@ -4,35 +4,31 @@ from dotenv import load_dotenv
 from app.core.file_format import ChunkMetadata, FileMetadata
 from pymongo import AsyncMongoClient
 
+
 _uri = os.getenv("MONGO_URI")
 client = AsyncMongoClient(_uri)
+database = client.get_database("unibase")
+unibase = database.get_collection("unibase_collection")
 
 async def record_chunk_metadata(file_meta: FileMetadata, chunk_meta: ChunkMetadata) -> None:
     new_chunk_dict = chunk_meta.model_dump()
     file_base_dict = file_meta.model_dump(exclude={"chunks"}) # Exclude the empty list
 
+    # Add document if it doesn't exist
     update_operation = {
         "$set": {
             f"chunk_list.{str(chunk_meta.chunk_index)}" : new_chunk_dict
         },
         "$setOnInsert": file_base_dict
     }
-
     try:
-        database = client.get_database("unibase")
-        unibase = database.get_collection("unibase_collection")
         query = { "_id": file_meta.file_name }
-
-        # Add document if it doesn't exist
         await unibase.update_one(query, update_operation, upsert=True)
     except Exception as e:
         raise Exception("Unable to find the document due to the following error: ", e)
-    
 
 async def get_chunk_metadata(file_name: str) -> list[str]:
     try:
-        database = client.get_database("unibase")
-        unibase = database.get_collection("unibase_collection")
         query = { "_id": file_name }
 
         # Fetch the document based on file_name
@@ -50,6 +46,20 @@ async def get_chunk_metadata(file_name: str) -> list[str]:
             chunk_id_list.append(chunk["provider_id"])
         
         return chunk_id_list
+    except Exception as e:
+            raise Exception("Unable to find the document due to the following error: ", e)
+
+async def delete_chunk_metadata(file_name: str) -> bool:
+    try:
+        query = { "_id": file_name }
+
+        # Delete the doc based on file_name
+        res = await unibase.delete_one(query)
+
+        if res.deleted_count > 0:
+             return True
+        return False
+
     except Exception as e:
             raise Exception("Unable to find the document due to the following error: ", e)
 
