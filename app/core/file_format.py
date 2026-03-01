ca205ab9d5a7ae2c05aeb1ef07b7963033b8ef5f
tn11 <tn11@illinois.edu>
2026-03-01 02:54:17 -0600

    Forgot to add new file format

--- app/core/file_format.py
+++ app/core/file_format.py
@@ -3,12 +3,16 @@ from typing import List
 
 class ChunkMetadata(BaseModel):
     chunk_index: int
+    chunk_name: str
     db_provider: str
     provider_id: str
     sha256: str
 
 class FileMetadata(BaseModel):
     file_name: str
+    content_type: str
     file_size: int
     chunk_size: int
+    num_chunks: int
+    file_sha256: str
     chunks: List[ChunkMetadata] = []
